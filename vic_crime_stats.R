82cf866cbc1be5a1f57d0bf9dc6a56cbb4cfb659
Brent-Morrison <brentjohnmorrison@hotmail.com>
2025-06-29 22:46:11 +1000

    Data munging

--- vic_crime_stats.R
+++ vic_crime_stats.R
@@ -2,28 +2,44 @@ library(sf)
 library(leaflet)
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(readxl)
+
 
 # Population data
 # https://www.vic.gov.au/sites/default/files/2019-08/Full-Report-Population-Diversity-in-LGAs-2016-Census-Web-version-30May18.PDF
+popn_data <- read.csv("vic_lga.csv")
 
-url <- "https://files.crimestatistics.vic.gov.au/2025-06/Indigenous_Data_Tables_LGA_Alleged_Offenders_Visualisation_Year_Ending_March_2025.xlsx"
-temp <- tempfile()
-download.file(url, temp, mode = "wb")
-data <- read_excel(path = temp, range = "Table 07!A1:F2958")
 
-# Optional: install.packages("sf") and others if not installed
 
-# Step 1: Download the LGA shapefile from the ABS
-# You can manually download from: 
-# https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/digital-boundary-files
-# File: "ASGS Edition 3, July 2021 to June 2026 - LGA boundaries - ESRI Shapefile"
+# Crime data
+if (file.exists("crimestatistics_vic.csv")) {
+  crimestatistics_vic <- read.csv(crimestatistics_vic, "crimestatistics_vic.csv")
+} else {
+  url <- "https://files.crimestatistics.vic.gov.au/2025-06/Indigenous_Data_Tables_LGA_Alleged_Offenders_Visualisation_Year_Ending_March_2025.xlsx"
+  temp <- tempfile()
+  download.file(url, temp, mode = "wb")
+  crimestatistics_vic <- read_excel(path = temp, range = "Table 07!A1:F2958")
+  crimestatistics_vic$retrieval_date <- Sys.Date()
+  write.csv(crimestatistics_vic, "crimestatistics_vic.csv")
+}
 
-# Assume you've extracted it to "data/LGA_2021_AUST.shp"
-# Adjust the path as needed
+crime_2016 <- crimestatistics_vic %>% 
+  filter(Year == 2016) %>% 
+  group_by(`Local Government Area`, `Indigenous Status`) %>% 
+  summarise(incidents = sum(`Alleged Offender Incidents`)) %>% 
+  pivot_wider(names_from = `Indigenous Status`, values_from = incidents)
+
+z <- left_join(crime_2016, popn_data[popn_data$rac != "rac", ], by = join_by(`Local Government Area` == vic_lga)) %>% 
+  mutate(region = if_else(metro_region == "na", paste0("Country - ", rac), paste(rac, metro_region, sep = " - "))) %>% 
+  group_by(region) %>% 
+  summarise(atsi = sum(`Aboriginal and/or Torres Strait Islander`, na.rm = T), ni = sum(`Non-Indigenous`, na.rm = T))
+
+# https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files
 lga <- st_read("data/LGA_2024_AUST_GDA2020.shp")
 
-# Step 2: Filter for Victoria only
+
+# Filter for Victoria only
 vic_lga <- lga %>%
   filter(STE_NAME21 == "Victoria")
 
